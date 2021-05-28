0f491714e4ee08bf7ef43dd2542b2d72172ae9a6
Bast <bast@fastmail.com>
2021-05-28 08:27:59 -0700

    Add basic homepage

---
+++ main.py
@@ -0,0 +1,20 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse, Response
+from starlette.routing import Route
+
+
+async def homepage(request):
+    return Response(
+        "Hello world! And welcome to Bast's Pixel Write Exchange!\n"
+        "All requests should have the 'Authorization' header set to a unique identifiable token that will be used for your balance.\n"
+        "GET /tasks to get the top ten highest paying tasks. You may provide ?minimum_pay=<float> to filter.\n"
+        '\tFormat: {"id": task_id, "pay": task_pay, "x": x_coord, "y": y_coord, "color": hex_color}'
+        "GET /tasks/<taskid> to claim a task. This claim will last 30 seconds.\n"
+        "POST /tasks/<taskid> to submit a task. We will verify whether the pixel has changed, and reward you with your payment.\n"
+        "POST /tasks to create a task. "
+    )
+
+
+app = Starlette(debug=True, routes=[
+    Route('/', homepage),
+])
