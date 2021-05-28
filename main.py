cf046d0149326670b109629cc4fd74ad8ae06462
Bast <bast@fastmail.com>
2021-05-28 08:52:37 -0700

    Add basic data model and some helpers

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import asyncio
+from datetime import datetime
+
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse, Response
 from starlette.routing import Route
+
+from pony import orm
 
 
 async def homepage(request):
@@ -15,6 +20,65 @@ async def homepage(request):
     )
 
 
-app = Starlette(debug=True, routes=[
-    Route('/', homepage),
-])
+db = orm.Database()
+
+
+class User(db.Entity):
+    id = orm.PrimaryKey(int, auto=True)
+    identifier = orm.Required(str, index=True)
+    money = orm.Required(float, sql_default=0)
+    total_tasks = orm.Required(int, sql_default=0)
+    requested_tasks = orm.Set('Task', reverse='reservation')
+    created_tasks = orm.Set('Task', reverse='creator')
+
+
+class Task(db.Entity):
+    id = orm.PrimaryKey(int, auto=True)
+    creator = orm.Required(User)
+    completed = orm.Required(bool, sql_default=False)
+    x = orm.Required(int)
+    y = orm.Required(int)
+    color = orm.Required(str)
+    pay = orm.Required(float)
+    reservation = orm.Optional(User)
+    reservation_expires = orm.Optional(datetime)
+    reservation_task_id = orm.Optional(int)  # name of the asyncio task we use to cancel auto-expire
+
+
+async def start_database():
+    db.bind(provider='sqlite', filename='data.db', create_db=True)
+    db.generate_mapping(create_tables=True)
+
+    with orm.db_session():
+        task_expiration_checks = orm.select(task for task in Task if task.reservation)
+        for task in task_expiration_checks:
+            assert task.reservation_expires is not None
+            assert task.reservation_task_id is not None
+            if task.reservation_expires < datetime.now():
+                task.reservation = None
+                task.reservation_expires = None
+                task.reservation_task_id = None
+            else:
+                asyncio.create_task(expire_task(task.id, task.reservation_expires))
+
+
+async def expire_task(task_id: int, time: datetime):
+    time_to_sleep = (datetime.now() - datetime).total_seconds()
+    await asyncio.sleep(time_to_sleep)
+    with orm.db_session():
+        task = Task[task_id]
+        if not task.completed:
+            task.reservation = None
+            task.reservation_task_id = None
+            task.reservation_expires = None
+        else:
+            return  # Successfully completed while we waited
+
+app = Starlette(
+    debug=True,
+    routes=[
+        Route('/', homepage),
+    ],
+    on_startup=[start_database],
+)
+orm.set_sql_debug(True)
