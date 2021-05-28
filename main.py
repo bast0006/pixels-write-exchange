497a8358262665f368850c9a41c012b4a11e7059
Bast <bast@fastmail.com>
2021-05-28 08:45:42 -0700

    Note how tokens are treated on homepage

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from starlette.routing import Route
 async def homepage(request):
     return Response(
         "Hello world! And welcome to Bast's Pixel Write Exchange!\n"
-        "All requests should have the 'Authorization' header set to a unique identifiable token that will be used for your balance.\n"
+        "All requests should have the 'Authorization' header set to a unique identifiable token of up to 30 characters that will be used for your balance. Surrounding spaces will be stripped.\n"
         "GET /tasks to get the top ten highest paying tasks. You may provide ?minimum_pay=<float> to filter.\n"
         '\tFormat: {"id": task_id, "pay": task_pay, "x": x_coord, "y": y_coord, "color": hex_color}'
         "GET /tasks/<taskid> to claim a task. This claim will last 30 seconds.\n"
